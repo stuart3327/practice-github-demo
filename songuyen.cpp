b55606a5d27b51d277c2f4b6e3b1980f62f1b782
Anna <ntnan3307@gmail.com>
2025-11-25 21:52:00 +0700

    Fix: add actual code content

---
+++ songuyen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b; cout << a << " " << b;
+    int tong = a + b;
+    int hieu = a - b;
+    int tich = a * b;
+    float thuong = 1.0*a / b;
+    int du = a % b;
+    cout << tong << " " << hieu << " " << tich << " " << thuong << " " << du << endl;
+    return 0;
+}
