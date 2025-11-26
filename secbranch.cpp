15c99b409ff58a37fc82212f9b98ed99ec344bea
Anna <ntnan3307@gmail.com>
2025-11-26 17:47:54 +0700

    Add secbranch.cpp

---
+++ secbranch.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << "Binh phuong cua tong 2 so la: " << (a + b)*(a + b);
+    return 0;
+}
